b0c19d72696876d6b1b17951e7f24ab9b39199ba
Rapheal Chizitere <www.rashyking05@gmail.com>
2024-10-05 21:44:16 +0100

    add prop prefixing

--- src/styled.tsx
+++ src/styled.tsx
@@ -113,8 +113,37 @@ const styled: StyledFunction = (tag: keyof JSX.IntrinsicElements) => {
       const styleString = `.${className} { ${generateStyleString(styles)} }`;
       styleSheetManager.addStyle(styleString);
 
+      const validProps = Object.keys((tag as any).propTypes || {});
+
+      // Handle props
+      const { prefixedProps, remainingProps } = Object.entries(props).reduce<{
+        prefixedProps: Record<string, any>;
+        remainingProps: Record<string, any>;
+      }>(
+        (acc, [key, value]) => {
+          if (
+            key !== "children" &&
+            key !== "className" &&
+            key !== "ref" &&
+            !key.startsWith("data-") &&
+            !validProps.includes(key)
+          ) {
+            if (typeof value === "function") {
+              acc.remainingProps[key] = value;
+            } else {
+              acc.prefixedProps[`data-${key.toLowerCase()}`] = value;
+            }
+          } else {
+            acc.remainingProps[key] = value;
+          }
+          return acc;
+        },
+        { prefixedProps: {}, remainingProps: {} }
+      );
+
       return React.createElement(tag, {
-        ...props,
+        ...prefixedProps,
+        ...remainingProps,
         ref,
         className: `${className} ${props.className || ""}`,
       });
