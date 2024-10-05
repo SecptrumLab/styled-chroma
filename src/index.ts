427d3f60e1d5788d01a24abec7a420cf9e26b1b5
Rapheal Chizitere <www.rashyking05@gmail.com>
2024-10-06 00:59:10 +0100

    add keyframes functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as styled } from "./styled";
 export { default as createGlobalStyle } from "./createGlobalStyle";
 export { default as css } from "./utils/css";
+export { default as keyframes } from "./utils/keyframes";
